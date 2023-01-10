4473e87aaf5f346ad6e7a52ae63866fb17632138
ETOUMI <aetoumi@gmail.com>
2023-01-10 17:47:43 +0000

    Actualisation du js

--- js/script.js
+++ js/script.js
@@ -14,10 +14,10 @@ let test = "";
 inputText.addEventListener("input", (ev) => {
     //console.log(test.replace("444"));
     let inputTextAll = ev.target.value;
-    letNewValue = '' + inputTextAll.charAt(inputTextAll.length - 1);
-    console.log(letNewValue);
-    if (letNewValue.search(regex) == -1) {
-        inputTextAll = inputTextAll.replace(letNewValue, '');
+    let newValue = '' + inputTextAll.charAt(inputTextAll.length - 1);
+    console.log(newValue);
+    if (newValue.search(regex) == -1) {
+        inputTextAll = inputTextAll.replace(newValue, '');
         ev.target.value = inputTextAll;
     }
 });
